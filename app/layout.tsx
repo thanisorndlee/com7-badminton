f99e008a74e85e0f38a28f25b7bf30ef5361c0c1
thanisorndlee <thanisornd47@gmail.com>
2026-06-30 09:09:39 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 {/* สปอนเซอร์ลอยเด่นไม่มีกรอบ */}
                 <div className="flex items-center gap-4">
                   <img src="/Sponsor.png" className="h-20 object-contain drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]" />
-                  <img src="/technique-logo.png" className="h-20 object-contain drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]" />
+                  <img src="/Sponsor.png" className="h-20 object-contain drop-shadow-[0_0_10px_rgba(255,255,255,0.2)]" />
                 </div>
               </div>
 
