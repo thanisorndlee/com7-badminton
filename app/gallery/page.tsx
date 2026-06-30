663785267d6f86f008e17e47c630d512c129d281
thanisorndlee <thanisornd47@gmail.com>
2026-06-30 09:46:39 +0700

    update

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -6,8 +6,8 @@ export default function GalleryPage() {
     '/gallery-1.jpg',
     '/gallery-2.jpg',
     '/gallery-3.jpg',
-    '/gallery-4.jpg',
-    '/gallery-5.jpg',
+    '/gallery-4.jpeg',
+    '/gallery-5.jpeg',
     '/gallery-6.jpg',
   ];
 
