5c67d99299da85b5393c46ae5a22129833be7598
thanisorndlee <thanisornd47@gmail.com>
2026-08-07 15:49:31 +0700

    uppdate

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -277,7 +277,7 @@ const handleUpload = async () => {
             ยังไม่มีภาพบรรยากาศการแข่งขัน
           </div>
         ) : (
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5">
             {photos.map((photo, index) => (
               <div
                 key={`${photo}-${index}`}
