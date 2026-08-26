2c77679455be856204b1218363ed6eada837bf06
thanisorndlee <thanisornd47@gmail.com>
2026-08-26 15:13:45 +0700

    update

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -928,7 +928,7 @@ const scoreB =
               key={`final-${i}`}
               className="absolute left-0"
               style={{
-                top: `${310 + i * 640}px`,
+                top: `${305 + i * 640}px`,
               }}
             >
               <BracketBox match={match} />
